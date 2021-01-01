5ab7f883feb2114a3956eba34c9c1044da30a1ce
PA <apavelr@gmail.com>
2021-01-01 18:33:15 -0500

    added paced

--- client/src/components/patient/data/heart_failure/Diagnosis.vue
+++ client/src/components/patient/data/heart_failure/Diagnosis.vue
@@ -1,74 +1,85 @@
 <template>
-   <item-block
-      :typesJson="typesJson"
-      itemType="diagnosis"
-      >
-      <template v-slot:displayData="props">
-         <v-list-item>
-            <v-list-item-content class="itemHeading">Start Date</v-list-item-content>
-            <v-list-item-content class="itemContent">{{props.item.dateStart}}</v-list-item-content>
-         </v-list-item>
-         <v-list-item>
-            <v-list-item-content class="itemHeading">End Date</v-list-item-content>
-            <v-list-item-content class="itemContent">{{props.item.dateEnd?props.item.dateEnd:'--'}}</v-list-item-content>
-         </v-list-item>
-         <v-list-item>
-            <v-list-item-content class="itemHeading">Type</v-list-item-content>
-            <v-list-item-content class="itemContent">{{props.item.details.type?props.item.details.type:'--'}}</v-list-item-content>
-         </v-list-item>
-         <v-list-item>
-            <v-list-item-content class="itemHeading">NYHA</v-list-item-content>
-            <v-list-item-content class="itemContent">{{props.item.details.NYHA?props.item.details.NYHA:'--'}}</v-list-item-content>
-         </v-list-item>
-      </template>
-      <template v-slot:dataEdit="{editedItem, types}">
-            <v-flex xs12>
-               <v-select
-                  v-model="editedItem.diagnosisName"
-                  filled
-                  label="Diagnosis"
-                  :items="typesJson.filter(x=>x.diagnosisName)"
-                  item-text="diagnosisName"
-                  item-value="diagnosisName"
-                  @change="editedItem.details={}"
-                  required :rules="[() => !!editedItem.diagnosisName || 'This field is required']"
-                  style="width: 50%"
-               ></v-select>
-            </v-flex>
+  <item-block :typesJson="typesJson" itemType="diagnosis">
+    <template v-slot:displayData="props">
+      <v-list-item>
+        <v-list-item-content class="itemHeading">
+          Start Date
+        </v-list-item-content>
+        <v-list-item-content class="itemContent">
+          {{ props.item.dateStart }}
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content class="itemHeading">End Date</v-list-item-content>
+        <v-list-item-content class="itemContent">
+          {{ props.item.dateEnd ? props.item.dateEnd : '--' }}
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content class="itemHeading">Type</v-list-item-content>
+        <v-list-item-content class="itemContent">
+          {{ props.item.details.type ? props.item.details.type : '--' }}
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content class="itemHeading">NYHA</v-list-item-content>
+        <v-list-item-content class="itemContent">
+          {{ props.item.details.NYHA ? props.item.details.NYHA : '--' }}
+        </v-list-item-content>
+      </v-list-item>
+    </template>
+    <template v-slot:dataEdit="{ editedItem }">
+      <!-- <template v-slot:dataEdit="{ editedItem, types }"> -->
+      <v-flex xs12>
+        <v-select
+          v-model="editedItem.diagnosisName"
+          filled
+          label="Diagnosis"
+          :items="typesJson.filter((x) => x.diagnosisName)"
+          item-text="diagnosisName"
+          item-value="diagnosisName"
+          @change="editedItem.details = {}"
+          required
+          :rules="[
+            () => !!editedItem.diagnosisName || 'This field is required',
+          ]"
+          style="width: 50%"
+        ></v-select>
+      </v-flex>
 
-            <v-flex xs12 sm4>
-               <v-text-field
-                  v-model="editedItem.dateStart"
-                  filled
-                  label="Start Date (YYYY-MM-DD)"
-                  prepend-inner-icon="mdi-calendar"
-                  v-mask="'####-##-##'"
-                  hide-details
-               ></v-text-field>
-            </v-flex>
+      <v-flex xs12 sm4>
+        <v-text-field
+          v-model="editedItem.dateStart"
+          filled
+          label="Start Date (YYYY-MM-DD)"
+          prepend-inner-icon="mdi-calendar"
+          v-mask="'####-##-##'"
+          hide-details
+        ></v-text-field>
+      </v-flex>
 
-            <v-flex xs12 sm4>
-               <v-text-field
-                  v-model="editedItem.endDate"
-                  filled
-                  label="End Date (YYYY-MM-DD)"
-                  prepend-inner-icon="mdi-calendar"
-                  v-mask="'####-##-##'"
-                  hide-details
-               ></v-text-field>
-            </v-flex>
+      <v-flex xs12 sm4>
+        <v-text-field
+          v-model="editedItem.endDate"
+          filled
+          label="End Date (YYYY-MM-DD)"
+          prepend-inner-icon="mdi-calendar"
+          v-mask="'####-##-##'"
+          hide-details
+        ></v-text-field>
+      </v-flex>
 
-            <v-flex xs12 sm4>
-               <v-text-field
-                  v-model="editedItem.treatingPhysician"
-                  hide-details
-                  filled
-                  label="Treating Physician (Last Name Only)"
-                  prepend-inner-icon="mdi-account-tie"
-               ></v-text-field>
-            </v-flex>
-      </template>
-   </item-block>
+      <v-flex xs12 sm4>
+        <v-text-field
+          v-model="editedItem.treatingPhysician"
+          hide-details
+          filled
+          label="Treating Physician (Last Name Only)"
+          prepend-inner-icon="mdi-account-tie"
+        ></v-text-field>
+      </v-flex>
+    </template>
+  </item-block>
 </template>
 
 <script>
@@ -76,16 +87,14 @@ import ItemBlock from '../ItemBlock'
 import DiagnosisTypes from './DiagnosisTypes'
 
 export default {
-   components: {ItemBlock},
-   data() {
-      return {
-         typesJson: DiagnosisTypes
-      }
-   }
+  components: { ItemBlock },
+  data() {
+    return {
+      typesJson: DiagnosisTypes,
+    }
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped type="sass">
-
-</style>
+<style scoped type="sass"></style>
--- client/src/components/patient/data/heart_failure/Other.vue
+++ client/src/components/patient/data/heart_failure/Other.vue
@@ -1,76 +1,90 @@
 /* eslint-disable vue/valid-template-root */
 <template>
-  <item :patientP="patientP" 
-    itemType="other" 
+  <item
+    :patientP="patientP"
+    itemType="other"
     itemTitle="Other"
     :tableHeaders="tableHeaders"
     :defaultItem="defaultItem"
-    sortByP = "otherDate"
-    :typesJson="otherTypes">
-    <template v-slot:dataEdit="{editedItem, types}">
-                <v-flex xs12 sm6>
-                  <v-select
-                    v-model="editedItem.otherName"
-                    filled
-                    label="Other Type"
-                    :items="types"
-                    item-text="otherName"
-                    item-value="otherName"
-                    @change="editedItem.details={}"
-                    required :rules="[() => !!editedItem.otherName || 'This field is required']"
-                  ></v-select>
-
-                </v-flex>
-                <v-flex xs6 sm6>
-                  <v-text-field
-                    v-model="editedItem.otherDate"
-                    filled
-                    label="Other Date (YYYY-MM-DD)"
-                    prepend-inner-icon="mdi-calendar"
-                    required
-                    v-mask="'####-##-##'"
-                    :rules="[() => !!editedItem.otherDate || 'This field is required']"
-                  ></v-text-field>
-                </v-flex>
+    sortByP="otherDate"
+    :typesJson="otherTypes"
+  >
+    <template v-slot:dataEdit="{ editedItem, types }">
+      <v-flex xs12 sm6>
+        <v-select
+          v-model="editedItem.otherName"
+          filled
+          label="Other Type"
+          :items="types"
+          item-text="otherName"
+          item-value="otherName"
+          @change="editedItem.details = {}"
+          required
+          :rules="[() => !!editedItem.otherName || 'This field is required']"
+        ></v-select>
+      </v-flex>
+      <v-flex xs6 sm6>
+        <v-text-field
+          v-model="editedItem.otherDate"
+          filled
+          label="Other Date (YYYY-MM-DD)"
+          prepend-inner-icon="mdi-calendar"
+          required
+          v-mask="'####-##-##'"
+          :rules="[() => !!editedItem.otherDate || 'This field is required']"
+        ></v-text-field>
+      </v-flex>
     </template>
     <template v-slot:dataTable="props">
-          <td class="eventName" style="white-space: nowrap; font-weight: bold;">{{ props.item.otherName }}</td>
-          <td class="eventName nowrap">{{ props.item.visibleDetail }}</td>
-          <td class="text-xs-center nowrap">{{ props.item.otherDate }}</td>
-          <td class="text-xs-center overflow-cell-wrapper">
-            <div class="overflow-cell">{{ props.item.comments }}</div>
-            </td>
+      <td class="eventName" style="white-space: nowrap; font-weight: bold">
+        {{ props.item.otherName }}
+      </td>
+      <td class="eventName nowrap">{{ props.item.visibleDetail }}</td>
+      <td class="text-xs-center nowrap">{{ props.item.otherDate }}</td>
+      <td class="text-xs-center overflow-cell-wrapper">
+        <div class="overflow-cell">{{ props.item.comments }}</div>
+      </td>
     </template>
   </item>
 </template>
 
 <script>
-
-import Item from "../Item"
-import OtherTypes from "./OtherTypes"
+import Item from '../Item'
+import OtherTypes from './OtherTypes'
 
 export default {
   props: ['patientP'],
-  components: {Item},
-  data () {
+  components: { Item },
+  data() {
     return {
       otherTypes: OtherTypes,
       tableHeaders: [
-        { text: "Other", value: "otherName", align: "center", width: "1%"},
-        { text: "Detail", value: "visibleDetail", align: "center", width: "1%"},
-        { text: "Date", value: "otherDate", align: "center", width: "1%"},
-        { text: "Comments", align: "center", value: "comments"},
-        { text: "Actions", align: "center", value: "name", sortable: false, width: "1%" }
+        { text: 'Other', value: 'otherName', align: 'center', width: '1%' },
+        {
+          text: 'Detail',
+          value: 'visibleDetail',
+          align: 'center',
+          width: '1%',
+        },
+        { text: 'Date', value: 'otherDate', align: 'center', width: '1%' },
+        { text: 'Comments', align: 'center', value: 'comments' },
+        {
+          text: 'Actions',
+          align: 'center',
+          value: 'name',
+          sortable: false,
+          width: '1%',
+        },
       ],
       defaultItem: {
-        otherName: "",
+        otherName: '',
         otherDate: null,
-        comments: "",
-        visibleDetail: "",
-        details: {}
+        comments: '',
+        visibleDetail: '',
+        details: {},
       },
     }
-  }
+  },
 }
 </script>
 
